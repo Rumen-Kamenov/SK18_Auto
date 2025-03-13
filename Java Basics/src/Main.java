c80a3ab9a7db0ecbd69304cdf0e98d1d4c86d48b
Rumen-Kamenov <rumini_bur@abv.bg>
2025-03-13 15:44:23 +0200

    commit new sout to main.class

--- Java Basics/src/Main.java
+++ Java Basics/src/Main.java
@@ -1,17 +1,8 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object for fied for username
-        Scanner IntAge = new Scanner(System.in);  // Create a Scanner object for field age
-
-        System.out.println("Enter username:"); // Print username text
-        String userName = myObj.nextLine();  // Read user input for username
-
-        System.out.println("Enter Age:");    // Print age text
-        int age = IntAge.nextInt();         // Read user input for Age
-
-        System.out.println("username is:" + userName);
-        System.out.println("Age is:" + age);
+        System.out.println("Hello world");
+        System.out.println("new changes");
     }
 }
