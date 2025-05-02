f348a8097f97558959a609459d90aba021869596
Rumen-Kamenov <rumini_bur@abv.bg>
2025-05-02 15:16:53 +0300

    simple exercise for validation

---
+++ Java Basics/src/ConditionalStatements/simpleCheckForNumberString.java
@@ -0,0 +1,20 @@
+package ConditionalStatements;
+
+import java.util.Scanner;
+
+public class simpleCheckForNumberString {
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter N:");
+
+        int n = myScanner.nextInt();
+        String str = String.valueOf(n);
+
+        if(-100 >= n && n <= 100){
+            System.out.println("Wrong answer");
+        } else {
+            System.out.println("Good job");
+        }
+    }
+}
